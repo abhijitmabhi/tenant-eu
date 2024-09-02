accf76f246200db3ea304a50a9e0cd7c258cbc57
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2024-09-02 09:15:45 +0200

    Initial commit

---
+++ src/main/java/com/ama/tenanteu/TenantEuApplication.java
@@ -0,0 +1,13 @@
+package com.ama.tenanteu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenantEuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenantEuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ama/tenanteu/TenantEuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ama.tenanteu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenantEuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
